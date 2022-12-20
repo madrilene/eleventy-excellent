55be1e15e461012797c4b74372afb7b59c5303d5
madrilene <hola@lenesaile.com>
2022-12-20 15:51:50 +0100

    strip unused exports from config

--- config/filters/index.js
+++ config/filters/index.js
@@ -14,48 +14,13 @@ const limit = (array, limit) => {
   return array.slice(0, limit);
 };
 
-/** Sorts the given array of objects by a string denoting chained key paths. */
-const sortByKey = (arrayOfObjects, keyPath, order = 'ASC') => {
-  const sorted = lodash.sortBy(arrayOfObjects, object => lodash.get(object, keyPath));
-  if (order === 'ASC') return sorted;
-  if (order === 'DESC') return sorted.reverse();
-  throw new Error(`Invalid sort order: ${order}`);
-};
-
 /** Returns all entries from the given array that match the specified key:value pair. */
 const where = (arrayOfObjects, keyPath, value) =>
   arrayOfObjects.filter(object => lodash.get(object, keyPath) === value);
 
-/** Returns the word count of the given string. */
-const wordCount = str => {
-  throwIfNotType(str, 'string');
-  const matches = str.match(/[\w\d’'-]+/gi);
-  return matches?.length ?? 0;
-};
-
 /** Converts the given markdown string to HTML, returning it as a string. */
 const toHtml = markdownString => {
   return markdownLib.renderInline(markdownString);
-};
-
-/** Divides the first argument by the second. */
-const dividedBy = (numerator, denominator) => {
-  if (denominator === 0) {
-    throw new Error(`Cannot divide by zero: ${numerator} / ${denominator}`);
-  }
-  return numerator / denominator;
-};
-
-/** Replaces every newline with a line break. */
-const newlineToBr = str => {
-  throwIfNotType(str, 'string');
-  return str.replace(/\n/g, '<br>');
-};
-
-/** Removes every newline from the given string. */
-const stripNewlines = str => {
-  throwIfNotType(str, 'string');
-  return str.replace(/\n/g, '');
 };
 
 /** Removes all tags from an HTML string. */
@@ -80,28 +45,6 @@ const toISOString = dateString => dayjs(dateString).toISOString();
 
 /** Formats a date using dayjs's conventions: https://day.js.org/docs/en/display/format */
 const formatDate = (date, format) => dayjs(date).format(format);
-
-/**
- * @param {*} collection - an array of collection items that are assumed to have either data.lastUpdated or a date property
- * @returns the most recent date of update or publication among the given collection items, or undefined if the array is empty.
- */
-const getLatestCollectionItemDate = collection => {
-  const itemsSortedByLatestDate = collection
-    .filter(item => !!item.data?.lastUpdated || !!item.date)
-    .sort((item1, item2) => {
-      const date1 = item1.data?.lastUpdated ?? item1.date;
-      const date2 = item2.data?.lastUpdated ?? item2.date;
-      if (dayjs(date1).isAfter(date2)) {
-        return -1;
-      }
-      if (dayjs(date2).isAfter(date1)) {
-        return 1;
-      }
-      return 0;
-    });
-  const latestItem = itemsSortedByLatestDate[0];
-  return latestItem?.data?.lastUpdated ?? latestItem?.date;
-};
 
 const minifyCss = code => new CleanCSS({}).minify(code).styles;
 
@@ -135,18 +78,12 @@ const mdInline = (content, opts) => {
 
 module.exports = {
   limit,
-  sortByKey,
-  where,
-  wordCount,
   toHtml,
+  where,
   toISOString,
   formatDate,
-  dividedBy,
-  newlineToBr,
-  stripNewlines,
-  stripHtml,
   toAbsoluteUrl,
-  getLatestCollectionItemDate,
+  stripHtml,
   minifyCss,
   mdInline
 };
