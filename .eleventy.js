6e7d1e0f02c1ad11d26f0851b783de8dbf0da65f
madrilene <hola@lenesaile.com>
2023-05-23 12:37:00 +0200

    repair wrapping menu, converting all sizing properties to logical properties, readjust spacing values

--- .eleventy.js
+++ .eleventy.js
@@ -46,6 +46,7 @@ const {slugifyString} = require('./config/utils');
 const {escape} = require('lodash');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
+const bundlerPlugin = require('@11ty/eleventy-plugin-bundle');
 
 module.exports = eleventyConfig => {
   // 	--------------------- Custom Watch Targets -----------------------
@@ -105,6 +106,7 @@ module.exports = eleventyConfig => {
   eleventyConfig.setLibrary('md', markdownLib);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(inclusiveLangPlugin);
+  eleventyConfig.addPlugin(bundlerPlugin);
 
   // 	--------------------- Passthrough File Copy -----------------------
   // same path
