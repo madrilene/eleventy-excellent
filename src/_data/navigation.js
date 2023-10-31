b06cf67caabfd7bb959039a94bc7e0f1bce00e70
madrilene <hola@lenesaile.com>
2023-10-31 16:29:19 +0100

    fix wrong navigation target

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,30 +1,30 @@
 module.exports = {
   top: [
     {
-      text: "About",
-      url: "/about/",
+      text: 'About',
+      url: '/about/'
     },
     {
-      text: "Get started",
-      url: "/get-started/",
+      text: 'Get started',
+      url: '/get-started/'
     },
     {
-      text: "Built with",
-      url: "/built-with/",
+      text: 'Built with',
+      url: '/built-with/'
     },
     {
-      text: "Blog",
-      url: "/blog/page-1/",
-    },
+      text: 'Blog',
+      url: '/blog/page-0/'
+    }
   ],
   bottom: [
     {
-      text: "Imprint",
-      url: "/imprint/",
+      text: 'Imprint',
+      url: '/imprint/'
     },
     {
-      text: "Privacy",
-      url: "/privacy/",
-    },
-  ],
+      text: 'Privacy',
+      url: '/privacy/'
+    }
+  ]
 };
