055aa784554e637a4dfc7a1eff7cfe8e3fc3155d
madrilene <hola@lenesaile.com>
2024-01-30 18:39:39 +0100

    new and changed fields in meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -7,41 +7,62 @@ module.exports = {
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
+  author: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+  authorAvatar: '/favicon.png', // for h-card, defaults to favicon
   authorEmail: '', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
+  creator: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
+  creatorEmail: 'hola@lenesaile.com', // i.e. hola@lenesaile.com
+  creatorWebsite: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com
+  creatorSocial: 'https://front-end.social/@lene', // i.e. https.://www.lenesaile.com
   themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
-  themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
+  themeBgColor: '#FBFBFB', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
-    opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
+    opengraph_default: '/assets/images/template/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
-      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
-    twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the site
-    mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
+      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es' // alt text for default meta image
   },
   blog: {
     // this is for the rss feed
-    name: 'My great Web Development Blog',
+    name: 'My Web Development Blog',
     description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'Tell the word what you are writing about in your blog. It will show up on feed readers.',
+    tagSingle: 'Tag',
+    tagPlural: 'Tags',
+    // feed links are looped over in the head.
+    feedLinks: [{title: 'Atom Feed', url: '/feed.xml', type: 'application/atom+xml'}],
+    pagination: 'Blog',
+    paginationPage: 'Page',
+    paginationPrevious: 'Previous',
+    paginationNext: 'Next'
   },
   pagination: {
     itemsPerPage: 20
   },
-  address: {
-    // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
-    street: '123 Main St.',
-    city: 'Ciudad',
-    state: 'Estado',
-    zip: '12345',
-    mobileDisplay: '+34 1234567',
-    mobileCall: ' +341234567',
-    email: 'hola@yoursite.com',
-    cif: ''
-  },
-  menu: {
+  navigation: {
+    ariaTop: 'Main',
+    ariaBottom: 'Complementary',
+    ariaSocial: 'Social',
     closedText: 'Menu'
-  }
+  },
+  themeSwitch: {
+    title: 'Theme',
+    light: 'light',
+    dark: 'dark'
+  },
+  greenweb: {
+    // this goues into src/common/greenweb.njk
+    providers: {
+      // if you want to add more than one, edit the array directly.
+      domain: 'netlify.com',
+      service: 'static-hosting'
+    },
+    credentials: {
+      // optional, eg: 	{ domain='my-org.com', doctype = 'webpage', url = 'https://my-org.com/our-climate-record'}
+      domain: '',
+      doctype: '',
+      url: ''
+    }
+  },
+  easteregg: true
 };
