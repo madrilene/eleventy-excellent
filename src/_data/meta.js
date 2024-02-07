a0b47b596015a1232765b6b7cd3355dd134c84c4
madrilene <hola@lenesaile.com>
2024-02-07 11:51:09 +0100

    rename to platform(s), to be more general

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -42,14 +42,14 @@ module.exports = {
   navigation: {
     ariaTop: 'Main',
     ariaBottom: 'Complementary',
-    ariaSocial: 'Social',
+    ariaPlatforms: 'Platforms',
     closedText: 'Menu'
   },
   themeSwitch: {
     title: 'Theme',
     light: 'light',
     dark: 'dark',
-		initial: 'select'
+    initial: 'select'
   },
   greenweb: {
     // this goues into src/common/greenweb.njk
