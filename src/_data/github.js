270f8ce6d527b9222c3e9b3c2188477243727719
Rigoberto L. Perez <rlperez@kablamo.me>
2024-09-05 21:30:08 -0400

    fix: 🐛 update more fields to be my name/accounts

--- src/_data/github.js
+++ src/_data/github.js
@@ -1,10 +1,9 @@
 import EleventyFetch from '@11ty/eleventy-fetch';
 
 export default async function () {
-  let url = 'https://api.github.com/users/madrilene/repos';
+  let url = 'https://api.github.com/users/rlperez/repos';
 
   // returning promise
-
   let data = await EleventyFetch(url, {
     duration: '1d',
     type: 'json'
