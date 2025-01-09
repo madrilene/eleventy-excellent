43d97641c07824761972931131a047159510c261
madrilene <hola@lenesaile.com>
2025-01-09 14:04:05 +0100

    remove markdown-it-eleventy-img, add markdown-it-attrs

--- src/_config/plugins/markdown.js
+++ src/_config/plugins/markdown.js
@@ -1,15 +1,14 @@
 import markdownIt from 'markdown-it';
+import markdownItAttrs from 'markdown-it-attrs';
 import markdownItPrism from 'markdown-it-prism';
 import markdownItAnchor from 'markdown-it-anchor';
 import markdownItClass from '@toycode/markdown-it-class';
 import markdownItLinkAttributes from 'markdown-it-link-attributes';
 import {full as markdownItEmoji} from 'markdown-it-emoji';
-import markdownItEleventyImg from 'markdown-it-eleventy-img';
 import markdownItFootnote from 'markdown-it-footnote';
 import markdownitMark from 'markdown-it-mark';
 import markdownitAbbr from 'markdown-it-abbr';
 import {slugifyString} from '../filters/slugify.js';
-import path from 'node:path';
 
 export const markdownLib = markdownIt({
   html: true,
@@ -18,6 +17,7 @@ export const markdownLib = markdownIt({
   typographer: true
 })
   .disable('code')
+  .use(markdownItAttrs)
   .use(markdownItPrism, {
     defaultLanguage: 'plaintext'
   })
@@ -44,43 +44,18 @@ export const markdownLib = markdownIt({
     }
   ])
   .use(markdownItEmoji)
-  .use(markdownItEleventyImg, {
-    imgOptions: {
-      widths: [440, 880, 1024],
-      urlPath: '/assets/images/',
-      outputDir: './dist/assets/images/',
-      formats: ['webp', 'jpeg']
-    },
-    globalAttributes: {
-      loading: 'lazy',
-      decoding: 'async',
-      sizes: '90vw'
-    },
-    // prepend src for markdown images
-    resolvePath: (filepath, env) => {
-      return path.join('src', filepath);
-    },
-    renderImage(image, attributes) {
-      const [Image, options] = image;
-      const [src, attrs] = attributes;
-
-      Image(src, options);
-
-      const metadata = Image.statsSync(src, options);
-      const imageMarkup = Image.generateHTML(metadata, attrs, {
-        whitespaceMode: 'inline'
-      });
-
-      const imageElement = attrs.title
-        ? `<figure class="flow">
-			${imageMarkup}
-					<figcaption>${attrs.title}</figcaption>
-				</figure>`
-        : `${imageMarkup}`;
-
-      return imageElement;
-    }
-  })
   .use(markdownItFootnote)
   .use(markdownitMark)
   .use(markdownitAbbr);
+// .use(md => {
+//   md.renderer.rules.image = (tokens, idx) => {
+//     const token = tokens[idx];
+//     const src = token.attrGet('src');
+//     const alt = token.content || '';
+//     const title = token.attrGet('title') || '';
+
+//     const imgTag = `<img src="${src}" alt="${alt}" loading="lazy" decoding="async">`;
+
+//     return title ? `<figure>${imgTag}<figcaption>${title}</figcaption></figure>` : imgTag;
+//   };
+// });
