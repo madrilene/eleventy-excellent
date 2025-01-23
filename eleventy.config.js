37cdc1915b4337654b1ea95cd12c1ddce9da36f7
madrilene <hola@lenesaile.com>
2025-01-23 07:21:28 +0100

    reinstate allPosts, as it is needed for OG images

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,7 +18,7 @@ dotenv.config();
 import yaml from 'js-yaml';
 
 //  config import
-import {showInSitemap, tagList} from './src/_config/collections.js';
+import {getAllPosts, showInSitemap, tagList} from './src/_config/collections.js';
 import events from './src/_config/events.js';
 import filters from './src/_config/filters.js';
 import plugins from './src/_config/plugins.js';
@@ -35,6 +35,7 @@ export default async function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('tags', 'tags.njk');
 
   //	---------------------  Collections
+  eleventyConfig.addCollection('allPosts', getAllPosts);
   eleventyConfig.addCollection('showInSitemap', showInSitemap);
   eleventyConfig.addCollection('tagList', tagList);
 
--- src/_config/collections.js
+++ src/_config/collections.js
@@ -1,4 +1,9 @@
-/** All markdown files as a collection for sitemap.xml */
+/** All blog posts as a collection. */
+export const getAllPosts = collection => {
+  return collection.getFilteredByGlob('./src/posts/**/*.md').reverse();
+};
+
+/** All relevant pages as a collection for sitemap.xml */
 export const showInSitemap = collection => {
   return collection.getFilteredByGlob('./src/**/*.{md,njk}');
 };
