af316331f0641a0153343d86b4322471f9dcd223
Sam Jackson <hipposareevil@gmail.com>
2025-03-03 13:12:54 +1300

    added new favicons and moving content to left

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,22 +1,21 @@
 export const url = process.env.URL || 'http://localhost:8080';
-export const siteName = 'Eleventy Excellent';
-export const siteDescription = 'Eleventy starter for building modern, resilient websites';
+export const siteName = 'willprogramforfood';
+export const siteDescription = 'Ramblings';
 export const siteType = 'Person'; // schema
 export const locale = 'en_EN';
 export const lang = 'en';
 export const skipContent = 'Skip to content';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
-  avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
+  name: 'Sam Jackson', 
+  avatar: '/icon-512x512.png', 
+  email: 'hipposareevil@gmail.com', 
+  website: 'https://www.willprogramforfood.com'
 };
 export const creator = {
-  name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
-  email: 'hola@lenesaile.com',
-  website: 'https://www.lenesaile.com',
-  social: 'https://front-end.social/@lene'
+  name: 'Sam Jackson', 
+  email: 'hipposareevil@gmail.com',
+  website: 'https://www.willprogramforfood.com',
+  social: 'https://hipposareevil.tumblr.com'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
 export const themeColor = '#dd4462'; // used in manifest, for example primary color value
@@ -27,8 +26,8 @@ export const opengraph_default_alt =
   "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
 export const blog = {
   // RSS feed
-  name: 'My Web Development Blog',
-  description: 'Tell the word what you are writing about in your blog. It will show up on feed readers.',
+  name: 'Ramblings',
+  description: 'willprogramforfood.com',
   // feed links are looped over in the head. You may add more to the array.
   feedLinks: [
     {
--- src/pages/about.md
+++ src/pages/about.md
@@ -1,5 +1,5 @@
 ---
-title: Aboutx
+title: About
 permalink: /about/index.html
 description: 'Eleventy Excellent is inspired bythe companion website of Andy Bell’s talk "Be the browser’s mentor, not its micromanager".'
 layout: page
