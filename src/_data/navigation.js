02eab8fff7e0ce9c53e8aafff027787be2f35708
Jay Cuthrell <jay@cuthrell.com>
2025-06-23 22:23:13 -0600

    update

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,14 +1,38 @@
 export default {
-  top: [
+   top: [
     {
-      text: 'Blog',
+      text: 'Archive',
       url: '/archive/'
+    },
+    {
+      text: 'Topics',
+      url: '/topics/'
+    },
+    {
+      text: 'About',
+      url: '/about/'
+    },
+    {
+      text: 'Sponsorship',
+      url: '/sponsorship/'
+    },
+    {
+      text: 'RSS',
+      url: '/rss.xml'
+    },
+    {
+      text: 'Hot Fudge Daily',
+      url: 'https://hot.fudge.org'
     }
   ],
   bottom: [
     {
+      text: 'Sponsorship',
+      url: '/sponsorship/'
+    },
+    {
       text: 'Disclosure',
-      url: 'https://jaycuthrell.com/disclosure/'
+      url: 'https://jaycuthrell.com/disclosure'
     }
   ]
 };
