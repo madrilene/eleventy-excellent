21c14d037f6b012bb84f3348d24e07ad7cac90be
Jay Cuthrell <jay@cuthrell.com>
2025-08-17 09:21:36 -0400

    daily push

--- eleventy.config.js
+++ eleventy.config.js
@@ -54,7 +54,7 @@ export default async function (eleventyConfig) {
     
   // Add the opengraph-unfurl plugin
   eleventyConfig.addPlugin(plugins.opengraphUnfurl, {
-    allowedDomains: ['github.com', 'techmeme.com', 'linkedin.com', 'en.wikipedia.org', 'cnet.com']
+    allowedDomains: ['github.com', 'techmeme.com', 'linkedin.com', 'en.wikipedia.org', 'brighttalk.com', 'prnewswire.com', 'cnet.com']
   });
   
   eleventyConfig.addPlugin(plugins.EleventyRenderPlugin);
