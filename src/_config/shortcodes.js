e9981ed613f4183c3e3d3da81b0e279d80307299
Jay Cuthrell <jay@cuthrell.com>
2025-10-05 13:26:34 -0400

    removing imageShortcodeSync

--- src/_config/shortcodes.js
+++ src/_config/shortcodes.js
@@ -1,6 +1,5 @@
 import {imageShortcode} from './shortcodes/image.js';
-import {imageShortcodeSync} from './shortcodes/image.js';
 
 import {svgShortcode} from './shortcodes/svg.js';
 
-export default {imageShortcode, imageShortcodeSync, svgShortcode};
+export default {imageShortcode, svgShortcode};
