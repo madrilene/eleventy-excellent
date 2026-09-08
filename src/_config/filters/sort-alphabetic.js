11b0824aa91141f86d1d21a8fb1afd87d3fb3eb5
madrilene <hola@lenesaile.com>
2026-09-08 18:56:45 +0200

    fix: update sortAlphabetically to be case-insensitive

--- src/_config/filters/sort-alphabetic.js
+++ src/_config/filters/sort-alphabetic.js
@@ -1,7 +1,7 @@
 export const sortAlphabetically = array => {
   return array.sort((a, b) => {
-    if (a.data.title < b.data.title) return -1;
-    if (a.data.title > b.data.title) return 1;
+    if (a.data.title.toLowerCase() < b.data.title.toLowerCase()) return -1;
+    if (a.data.title.toLowerCase() > b.data.title.toLowerCase()) return 1;
     return 0;
   });
 };
